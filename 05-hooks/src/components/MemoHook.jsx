d666a4798d567d12002176d8eb83dec38cfab0ff
srudy <srudyjj@gmail.com>
2024-09-01 13:27:05 +0200

    useMemo

---
+++ 05-hooks/src/components/MemoHook.jsx
@@ -0,0 +1,30 @@
+import { useMemo, useState } from "react"
+
+export default function MemoHook() {
+  const [text, setText] = useState("");
+  const [person, setPerson] = useState("");
+
+  const isPersonCoolFunc = () => {
+    for (let index = 0; index < 1000000000; index++) {}
+    if (person === "Laith") return "not cool"
+    else if (person === "Shelby") return "very cool"
+    else if (person === "Sam") return "moderately cool"
+    else return ""
+  }
+
+  const isPersonCool = useMemo(isPersonCoolFunc, [person])
+
+  return (
+    <div>
+      <input type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)} />
+      <div>
+        <button onClick={() => setPerson("Laith")} >Laith</button>
+        <button onClick={() => setPerson("Shelby")} >Shelby</button>
+        <button onClick={() => setPerson("Sam")} >Sam</button>
+      </div>
+      <p>{isPersonCool}</p>
+    </div>
+  )
+}
