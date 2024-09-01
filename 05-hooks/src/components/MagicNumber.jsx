2761605e395cc40cf62b15c432f93fddf2e026f2
srudy <srudyjj@gmail.com>
2024-09-01 10:11:51 +0200

    useRef counter

---
+++ 05-hooks/src/components/MagicNumber.jsx
@@ -0,0 +1,41 @@
+import { useState, useRef } from "react";
+
+export default function MagicNumber() {
+
+  const magicNumber = 5;
+  const secondMagicNumber = 10;
+
+  const [messages, setMessages] = useState({
+    p: "",
+    button: "Click me until you reached the magic number"
+  })
+
+  const count = useRef(0);
+
+  console.log("The component is rerendering");
+  
+
+  const handleClick = () => {
+    count.current = count.current + 1;
+    if (count.current === magicNumber) {
+      setMessages({
+        p: "You reached it!",
+        button: "Click me until you reach the second Magic number"
+      })
+    } else if (count.current === secondMagicNumber) {
+      setMessages({
+        p: "You reached it again!",
+        button: "Stop clicking me!"
+      })
+    }
+  }
+
+  // You reached it!
+
+  return (
+    <div>
+      {messages.p && <p>{messages.p}</p>}
+      <button onClick={handleClick}>{messages.button}</button>
+    </div>
+  )
+}
