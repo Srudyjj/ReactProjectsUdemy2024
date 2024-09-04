de14ac7bb78ad0d817f06b0f2c121ec955e4d87b
srudy <srudyjj@gmail.com>
2024-09-04 16:31:37 +0200

    07: Add routing

---
+++ 07-netflix/client/src/components/NavBar.tsx
@@ -0,0 +1,32 @@
+const tabs = [
+  "Home",
+  "Series",
+  "Films",
+  "New & Popular",
+  "My List",
+  "Browse by Languages",
+];
+
+export default function NavBar() {
+  return (
+    <nav className="w-full fixed z-40">
+      <div className="px-16 py-6 flex items-center">
+        <img
+          className="h-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="logo"
+        />
+        <div className="flex gap-7 ml-8">
+          {tabs.map((tab) => (
+            <div
+              key={tab}
+              className="text-white hover:text-gray-300 cursor-pointer"
+            >
+              <p>{tab}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+}
